8db754c78c41fc72715c1c0347dda7658f450def
Dotan Simha <dotansimha@gmail.com>
2016-01-28 16:48:42 +0200

    Try to fix the css

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader'
-      },
-      {
-        test: /\.less$/,
-        loader: "style!css!less"
       }
     ]
   }
